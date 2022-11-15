b724542a7566c56d4e179bbaa4accfc86a0d914c
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-16 00:25:59 +0500

    displays the third digit of the given number, or reports that there is no third digit

---
+++ 2/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+Console.WriteLine ("Введите любое число"); 
+
+int InputNumber (string message)
+{
+    Console.Write (message);
+    return Convert.ToInt32 (Console.ReadLine ());
+}
+
+int numIn = InputNumber ($"Введите число: ");
+
+if (numIn / 100 == 0)
+    {
+    Console.WriteLine ("Третьей цифры нет");
+    }
+    else
+    {
+        while (numIn > 1000)
+        {
+            numIn = numIn / 10;
+        }
+        Console.WriteLine ($"Третья цифра: {numIn % 10}"); 
+    }
