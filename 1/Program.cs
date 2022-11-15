91038e34f7e254dff05cf49cf3f52bb95ba19e6b
Tatyana <tatyana.volkova1984@mail.ru>
2022-11-16 00:19:22 +0500

    takes a three-digit number as input and displays the second digit of this number as output.

---
+++ 1/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+Console.WriteLine("Введите любое трехзначное число");
+       while(true)
+    {
+        string number = Console.ReadLine();
+        if(!number.Equals("exit"))
+            Console.WriteLine("{0}->{1}",number, number[1]);
+        else
+            break;
+    }
